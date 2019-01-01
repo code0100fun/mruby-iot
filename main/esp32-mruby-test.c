41fd3b2875463c3aff0f2943c2df8c0b049dfb57
Chase McCarthy <chase@code0100fun.com>
2019-01-01 15:05:11 -0600

    Add SSD1306 component

--- main/esp32-mruby-test.c
+++ main/esp32-mruby-test.c
@@ -1,44 +1,127 @@
-/* Blink Example
+/**
+ * Copyright (c) 2017-2018 Tara Keeling
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "ssd1306.h"
+#include "ssd1306_draw.h"
+#include "ssd1306_font.h"
+#include "ssd1306_default_if.h"
 
-/* Can run 'make menuconfig' to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define USE_I2C_DISPLAY
+//#define USE_SPI_DISPLAY
 
-void blink_task(void *pvParameter)
-{
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+#if defined USE_I2C_DISPLAY
+    static const int I2CDisplayAddress = 0x3C;
+    static const int I2CDisplayWidth = 128;
+    static const int I2CDisplayHeight = 64;
+    static const int I2CResetPin = -1;
+
+    struct SSD1306_Device I2CDisplay;
+#endif
+
+#if defined USE_SPI_DISPLAY
+    static const int SPIDisplayChipSelect = 15;
+    static const int SPIDisplayWidth = 128;
+    static const int SPIDisplayHeight = 64;
+    static const int SPIResetPin = 5;
+
+    struct SSD1306_Device SPIDisplay;
+#endif
+
+static struct SSD1306_Device* Displays[ ] = {
+#if defined USE_I2C_DISPLAY
+    &I2CDisplay,
+#endif
+#if defined USE_SPI_DISPLAY
+    &SPIDisplay,
+#endif
+    NULL
+};
+
+const int DisplayCount = sizeof( Displays ) / sizeof( Displays[ 0 ] );
+
+const struct SSD1306_FontDef* FontList[ ] = {
+    &Font_droid_sans_fallback_11x13,
+    &Font_droid_sans_fallback_15x17,
+    &Font_droid_sans_fallback_24x28,
+    &Font_droid_sans_mono_7x13,
+    &Font_droid_sans_mono_13x24,
+    &Font_droid_sans_mono_16x31,
+    &Font_liberation_mono_9x15,
+    &Font_liberation_mono_13x21,
+    &Font_liberation_mono_17x30,
+    NULL
+};
+
+const int FontCount = sizeof( FontList ) / sizeof( FontList[ 0 ] );
+
+void SetupDemo( struct SSD1306_Device* DisplayHandle, const struct SSD1306_FontDef* Font );
+void SayHello( struct SSD1306_Device* DisplayHandle, const char* HelloText );
+
+bool DefaultBusInit( void ) {
+    #if defined USE_I2C_DISPLAY
+        assert( SSD1306_I2CMasterInitDefault( ) == true );
+        assert( SSD1306_I2CMasterAttachDisplayDefault( &I2CDisplay, I2CDisplayWidth, I2CDisplayHeight, I2CDisplayAddress, I2CResetPin ) == true );
+    #endif
+
+    #if defined USE_SPI_DISPLAY
+        assert( SSD1306_SPIMasterInitDefault( ) == true );
+        assert( SSD1306_SPIMasterAttachDisplayDefault( &SPIDisplay, SPIDisplayWidth, SPIDisplayHeight, SPIDisplayChipSelect, SPIResetPin ) == true );
+    #endif
+
+    return true;
 }
 
-void app_main()
-{
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+void FontDisplayTask( void* Param ) {
+    struct SSD1306_Device* Display = ( struct SSD1306_Device* ) Param;
+    int NextFontTime = 0;
+    int i = 0;
+
+    if ( Param != NULL ) {
+        for ( i = 0; i < FontCount && FontList[ i ] != NULL; i++ ) {
+            if ( FontList[ i ] != NULL ) {
+                SSD1306_SetFont( Display, FontList[ i ] );
+                NextFontTime = time( NULL ) + 2;
+
+                while ( time( NULL ) < NextFontTime ) {
+                    SSD1306_Clear( Display, SSD_COLOR_BLACK );
+                    SSD1306_FontDrawAnchoredString( Display, TextAnchor_Center, "Hello!", SSD_COLOR_WHITE );
+                    SSD1306_Update( Display );
+
+                    vTaskDelay( pdMS_TO_TICKS( 100 ) );
+                }
+
+                printf( "On to next font\n" );
+            }
+        }
+    }
+
+    printf( "Finished!\n" );
+    vTaskDelete( NULL );
+}
+
+void app_main( void ) {
+    int i = 0;
+
+    printf( "Ready...\n" );
+
+    if ( DefaultBusInit( ) == true ) {
+        printf( "BUS Init lookin good...\n" );
+        printf( "Drawing.\n" );
+
+        for ( i = 0; i < DisplayCount; i++ ) {
+            if ( Displays[ i ] != NULL ) {
+                xTaskCreate( FontDisplayTask, "FontDisplayTask", 4096, Displays[ i ], 1, NULL );
+            }
+        }
+    }
 }
